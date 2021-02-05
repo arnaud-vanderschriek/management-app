fda2c76b4bb3324aff09fc9d41a430b1265fc1f2
arnoo <arnaud>
2021-02-05 20:48:45 +0100

    modif

--- front/tma/src/components/users/state/models/users.ts
+++ front/tma/src/components/users/state/models/users.ts
@@ -1,8 +1,11 @@
 import { createModel } from "@rematch/core";
+import { Toastify } from '../../../helpers/Toastify';
+import { apiService } from '../../../http/service';
 
-type UsersSate = {
+type UsersState = {
   list: [],
   linkList: string,
+  usersData: {},
   isModalOpened: boolean,
 }
 
@@ -10,16 +13,26 @@ export const users = createModel({
     state: {
       list: [],
       linkList: '',
+      usersData: {},
       isModalOpened: false,
     },
     reducers: {
-      setIsModalOpened: (state: UsersSate, payload: boolean): UsersSate => ({...state, isModalOpened: payload}),
-      setLinkList: (state: UsersSate, payload: string): UsersSate => ({
-        ...state, linkList: payload}),
+      setIsModalOpened: (state: UsersState, payload: boolean): UsersState => ({...state, isModalOpened: payload}),
+      setLinkList: (state: UsersState, payload: string): UsersState => ({...state, linkList: payload}),
+      setDataUsers: (state: UsersState, payload: UsersState): UsersState => ({...state, usersData: payload})
     },
     effects: {
       async fetchDataProject(): Promise<void> {
         
+      },
+      async fetchDataUser(): Promise<void> {
+        try {
+          const { data } = await apiService.get('/users/getUsers')
+
+        } catch (error){
+          (new Toastify()).error(`Failed to login. ${error.message}`);
+        }
+
       }
     }
 })
